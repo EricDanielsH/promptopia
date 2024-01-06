babb49e531051aa6ceae1083e58b4c3e0ed813e3
EricDanielsH <e.danielshuaman@lancaster.ac.uk>
2024-01-06 23:29:52 +0000

    Fix bug

--- app/api/users/[id]/posts/route.js
+++ app/api/users/[id]/posts/route.js
@@ -6,11 +6,10 @@ export const GET = async (request, { params }) => {
   try {
     await connectToDB();
 
-    const prompts = await Prompt.find({ creator: params.id }).populate(
-      "creator"
-    );
+    const prompts = await Prompt.find({ creator: params.id }).populate("creator");
     return new Response(JSON.stringify(prompts), { status: 200 });
   } catch (error) {
     return new Response("Failed to get prompts of a profile", { status: 500 });
   }
 };
+  
